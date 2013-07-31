9ac42e2679c382e7d941a087cc40db60284c802a
Shuichi Aizawa <sa396@columbia.edu>
2013-07-31 19:59:20 -0400

    condensed movePiece() logic

--- pieces.js
+++ pieces.js
@@ -28,7 +28,7 @@ function rotatePiece(pieceRow, pieceCol, row, col) {
 }
 
 function movePiece(pieceRow, pieceCol, row, col) {
-	if (pieceRow >= 0 && pieceCol >= 0 && checkMove(pieceRow, pieceCol, row, col)) {
+	if (checkMove(pieceRow, pieceCol, row, col)) {
 		grid[row][col].player = grid[pieceRow][pieceCol].player;
 		grid[row][col].rot = grid[pieceRow][pieceCol].rot;
 		grid[pieceRow][pieceCol].player = -1;
@@ -37,47 +37,29 @@ function movePiece(pieceRow, pieceCol, row, col) {
 }
 
 function checkMove(pieceRow, pieceCol, row, col) {
-
-	// Clicked-on-piece check
-	if (pieceRow == -1 || pieceCol == -1) {
+	if (pieceRow < 0 || pieceCol < 0 || row < 0 || row >= 15 || col < 0 || col >= 21) {	// bounds
 		return false;
 	}
 
-	// Adjacent-square check
-	if (grid[pieceRow][pieceCol].cell != 3 && Math.abs(pieceRow-row) + Math.abs(pieceCol-col) > 1) {
+	if (grid[row][col].cell < 0 || grid[row][col].cell == 3) {	// invalid-sqaure
 		return false;
 	}
 
-	// On-board check
-	if (row < 0 || row >= 15 || col < 0 || col >= 21) {
+	if (grid[row][col].player >= 0 && (grid[row][col].player - grid[pieceRow][pieceCol].player)%2 == 0) {	// occupied-by-same-team
 		return false;
 	}
 
-	// Invalid-sqaure check
-	if (grid[row][col].cell < 0) {
+	if (grid[row][col].cell == 1 && (grid[row][col].zone - grid[pieceRow][pieceCol].player)%2 != 0 ) {	// opponent-win-square
 		return false;
 	}
 
-	// Occupied-by-same-team check
-	if (grid[row][col].player >= 0 && (grid[row][col].player - grid[pieceRow][pieceCol].player)%2 == 0) {
+	if (grid[pieceRow][pieceCol].cell != 3 && Math.abs(pieceRow-row) + Math.abs(pieceCol-col) > 1) {	// adjacent-square
 		return false;
 	}
 
-	// Routed cell check
-	if (grid[row][col].cell == 3) {
-		return false;
-	}
-
-	// Opponent-win-square check
-	if (grid[row][col].cell == 1 && (grid[row][col].zone - grid[pieceRow][pieceCol].player)%2 != 0 ) {
-		return false;
-	}
-
-	// Routed-to-respawn check
-	if (grid[pieceRow][pieceCol].cell == 3 && (grid[row][col].cell != 2 || grid[pieceRow][pieceCol].player != grid[row][col].zone) ) {
+	if (grid[pieceRow][pieceCol].cell == 3 && (grid[row][col].cell != 2 || grid[pieceRow][pieceCol].player != grid[row][col].zone) ) {	// routed-to-respawn
 		return false;
 	}
 
 	return true;
-
 }
