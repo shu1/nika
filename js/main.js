5c865ef79decc73eed4d045c57b5bb60fef4a0ad
Shuichi Aizawa <sa396@columbia.edu>
2015-01-31 20:58:27 -0500

    No analytics for WiiU

--- js/main.js
+++ js/main.js
@@ -642,9 +642,11 @@ function drawHud(context, sceneIndex) {
 		};
 }());
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-42200724-1', 'auto');
-ga('send', 'pageview');
+if (!window.nwf) {
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-42200724-1', 'auto');
+	ga('send', 'pageview');
+}
