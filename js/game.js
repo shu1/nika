58e1e55a9e6c6ce399ddf5c4a15e0cf7ec020202
harimohanraj89 <hari.mohanraj89@gmail.com>
2015-05-26 22:44:29 -0400

    added testAnimation() to playback all animation combinations

--- js/game.js
+++ js/game.js
@@ -336,6 +336,35 @@ function setIdleAnimation(player) {
 	}
 }
 
+function testAnimation(idx) {
+	var anims = [
+		[0,"push",1,"pushed"],[0,"rout",1,"routed"],[0,"rout",1,"pushed"],
+		[0,"push",3,"pushed"],[0,"rout",3,"routed"],[0,"rout",3,"pushed"],
+		[2,"push",1,"pushed"],[2,"rout",1,"routed"],[2,"rout",1,"pushed"],
+		[2,"push",3,"pushed"],[2,"rout",3,"routed"],[2,"rout",3,"pushed"],
+		[1,"push",0,"pushed"],[1,"rout",0,"routed"],[1,"rout",0,"pushed"],
+		[1,"push",2,"pushed"],[1,"rout",2,"routed"],[1,"rout",2,"pushed"],
+		[3,"push",0,"pushed"],[3,"rout",0,"routed"],[3,"rout",0,"pushed"],
+		[3,"push",2,"pushed"],[3,"rout",2,"routed"],[3,"rout",2,"pushed"],
+		[0,"rally",2,"rally"],[1,"rally",3,"rally"]
+	]
+
+	if (idx != undefined) {
+		console.log(idx + " : " + anims[idx].join(" "));
+		playAnimation(anims[idx][0], anims[idx][1]);
+		playAnimation(anims[idx][2], anims[idx][3]);
+	}
+	else {
+		anims.forEach(function(anim, idx) {
+			setTimeout(function() {
+				console.log(idx + " : " + anim.join(" "));
+				playAnimation(anim[0], anim[1]);
+				playAnimation(anim[2], anim[3]);
+			}, idx * 2000);
+		});
+	}
+}
+
 function menuDebug(index) {
 	switch (index) {
 	case 0:
