6c2e4491f6608b65c26617b289076dfea73a51ae
Shuichi Aizawa <sa396@columbia.edu>
2015-08-10 03:43:24 -0400

    Refactor mouseDown/mouseMove to consolidate repeated code.

--- js/input.js
+++ js/input.js
@@ -49,30 +49,10 @@ function mouseDown(event) {
 			var x = (inputMan.x - scene.x) / scene.scale;
 			var y = (inputMan.y - scene.y) / scene.scale;
 
-			if (gameMan.screen == "rules") {
-				menus["rules"] = -1;
-				if (y > (scene.height - drawMan.arrowHeight)/2 && y < (scene.height + drawMan.arrowHeight)/2) {
-					if (x > scene.width - drawMan.arrowWidth*1.5 && gameMan.rules < rulePages-1) {
-						menus["rules"] = 1;
-						handled = true;
-					}
-					else if (x < drawMan.arrowWidth*1.5 && gameMan.rules > 0) {
-						menus["rules"] = 0;
-						handled = true;
-					}
-				}
-			}
-			else if (gameMan.screen == "title") {
-				x -= (scene.width - drawMan.screenWidth)/2;	// offset to coordinates of image
-				y -= (scene.height - drawMan.screenHeight)/2 + 282;
+			handled = handleScreen(scene, x, y);
 
-				if (x > 128 && x < 528 && y > 0 && y < drawMan.activeHeight*6) {
-					menus["title"] = Math.floor(y / drawMan.activeHeight);
-					handled = true;
-				}
-			}
-			else if (gameMan.screen == "option") {
-				x -= (scene.width - drawMan.screenWidth)/2 + 156;
+			if (gameMan.screen == "option") {
+				x -= (scene.width - drawMan.screenWidth)/2 + 156;	// offset to near x of volume line
 				y -= (scene.height - drawMan.screenHeight)/2;
 
 				var radius = 96;	// bigger radius for fat fingers
@@ -210,6 +190,32 @@ function getRowCol(scene) {
 	hudMan.inputText = inputMan.row + "," + inputMan.col;
 }
 
+function handleScreen(scene, x, y) {
+	if (gameMan.screen == "rules") {
+		menus["rules"] = -1;
+		if (y > (scene.height - drawMan.arrowHeight)/2 && y < (scene.height + drawMan.arrowHeight)/2) {
+			if (x > scene.width - drawMan.arrowWidth*1.5 && gameMan.rules < rulePages-1) {
+				menus["rules"] = 1;
+				return true;
+			}
+			else if (x < drawMan.arrowWidth*1.5 && gameMan.rules > 0) {
+				menus["rules"] = 0;
+				return true;
+			}
+		}
+	}
+	else if (gameMan.screen == "title") {
+		x -= (scene.width - drawMan.screenWidth)/2;
+		y -= (scene.height - drawMan.screenHeight)/2 + 282;	// offset to top of menu
+		if (x > 128 && x < 528 && y > 0 && y < drawMan.activeHeight*6) {
+			menus["title"] = Math.floor(y / drawMan.activeHeight);
+			return true;
+		}
+	}
+
+	return false;
+}
+
 function mouseMove(event) {
 	if (inputMan.touchID >= 0) {
 		getXY(event);
@@ -234,29 +240,10 @@ function mouseMove(event) {
 			var y = (inputMan.y - scene.y) / scene.scale;
 			hudMan.inputText = Math.floor(x) + "," + Math.floor(y);
 
-			if (gameMan.screen == "rules") {
-				menus["rules"] = -1;
-				if (y > (scene.height - drawMan.arrowHeight)/2 && y < (scene.height + drawMan.arrowHeight)/2) {
-					if (x > scene.width - drawMan.arrowWidth*1.5 && gameMan.rules < rulePages-1) {
-						menus["rules"] = 1;
-						handled = true;
-					}
-					else if (x < drawMan.arrowWidth*1.5 && gameMan.rules > 0) {
-						menus["rules"] = 0;
-						handled = true;
-					}
-				}
-			}
-			else if (gameMan.screen == "title") {
-				x -= (scene.width - drawMan.screenWidth)/2;	// offset to coordinates of image
-				y -= (scene.height - drawMan.screenHeight)/2 + 282;
-				if (x > 128 && x < 528 && y > 0 && y < drawMan.activeHeight*6) {
-					menus["title"] = Math.floor(y / drawMan.activeHeight);
-					handled = true;
-				}
-			}
-			else if (gameMan.screen == "option") {
-				x -= (scene.width - drawMan.screenWidth)/2 + 156;
+			handled = handleScreen(scene, x, y);
+
+			if (gameMan.screen == "option") {
+				x -= (scene.width - drawMan.screenWidth)/2 + 156;	// offset to near x of volume line
 				y -= (scene.height - drawMan.screenHeight)/2;
 
 				if (inputMan.drag == "music") {
