f49c83211ebcd1fb5104634b10fabefd4cfe5b7d
harimohanraj89 <hari.mohanraj89@gmail.com>
2015-09-03 01:07:23 -0400

    pass linter

--- js/aiNeil.js
+++ js/aiNeil.js
@@ -57,14 +57,13 @@ var aiWeights = [
 	}
 ];
 
-var aiP = aiWeights[0]; //Alias?
+var aiP = aiWeights[0]; // Alias?
 
 function newState (){
-	var s = {
-		value : 0, //Value of state after board state adjustments
-		board : grid //grid object, state of board after this move, will be applied to board upon ai exit.
+	return {
+		value : 0,   // value of state after board state adjustments
+		board : grid // grid object, state of board after this move, will be applied to board upon ai exit.
 	};
-	return s;
 }
 
 function aiNeil(aiNum){
@@ -73,14 +72,13 @@ function aiNeil(aiNum){
 	var pieces = [];
 	pieces = getAIPieces();
 
-	//Checks if any pieces are close enough to win.
+	// Checks if any pieces are close enough to win.
 	checkIfPiecesCanWin(pieces);
 
-	//Creates the default state in which all other states are compared to.
+	// Creates the default state in which all other states are compared to.
 	var defaultState = newState();
-	getValue(defaultState,pieces);
-	// console.log("Default Value " + defaultState.value);
-	var bestState = defaultState; //Stores best state, which is default at this point. Altough we might be better off just setting best state.
+	getValue(defaultState, pieces);
+	var bestState = defaultState; // Stores best state, which is default at this point. Altough we might be better off just setting best state.
 
 	var rallySpots = [];
 	for (var row = 0 ; row < 15; ++row) {
@@ -91,185 +89,207 @@ function aiNeil(aiNum){
 		}
 	}
 
-	//EACH PIECE CHECKING =============
-	for(var i = 0; i<6; i++){
-		if(pieces[i].kind == 3){
-			for(var j = 0; j<rallySpots.length; j++){
-				//Get rally spots
+	// EACH PIECE CHECKING =============
+	for(var i = 0; i < 6; i++) {
+		if(pieces[i].kind == 3) {
+			for(var j = 0; j < rallySpots.length; j++) {
+				// Get rally spots
 				phalanx = [pieces[i]];
 				var rally = getRallyArguments(pieces[i],rallySpots[j]);
-				//debugger;
-				movePiece(rally.pRow,rally.pCol,rally.tRow,rally.tCol,true);
+				movePiece(rally.pRow, rally.pCol, rally.tRow, rally.tCol, true);
 				pieces = getAIPieces();
+
 				var temp = copyState(defaultState);
-				getValue(temp,pieces);
-				// console.log(temp.value);
-				if(temp.value>bestState.value){
-					bestState=temp;
-					// console.log("Found better state by RALLYING with a value of: " + bestState.value);
+				getValue(temp, pieces);
+
+				if(temp.value > bestState.value) {
+					bestState = temp;
 				}
-				setGrid(defGrid,grid);
+
+				setGrid(defGrid, grid);
 				pieces = getAIPieces();
 			}
 		}
-		else{
+		else {
 			// Do normal move check, which means rotation, then movement.
 			origRot = pieces[i].rot;
-			for (var rot = 0; rot<4; rot++){
-				if(rot!=origRot){
-					pieces[i].rot=rot;
+			for (var rot = 0; rot < 4; rot++){
+				if(rot != origRot){
+					pieces[i].rot = rot;
 					var temp = copyState(defaultState);
-					getValue(temp,pieces);
-					if(temp.value>bestState.value){
-						bestState=temp;
-						// console.log("Found better state by ROTATION with a value of: " + bestState.value);
+					getValue(temp, pieces);
+					if(temp.value > bestState.value){
+						bestState = temp;
 					}
 				}
 			}
+
 			setGrid(defGrid,grid);
 			pieces = getAIPieces();
+
 			for (var dir = 0; dir<4; dir++){
-				//Check moves in each direction
-				phalanx=[pieces[i]];
+				// Check moves in each direction
+				phalanx = [pieces[i]];
 
 				var move = getMoveArguments(phalanx[0],dir);
 				var origRot = phalanx[0].rot;
 
 				movePiece(move.pRow,move.pCol,move.tRow,move.tCol,true);
 				pieces = getAIPieces();
+
 				var temp = copyState(defaultState);
-				getValue(temp,pieces);
+				getValue(temp, pieces);
+
 				if(temp.value>bestState.value){
-					bestState=temp;
-					// console.log("Found better state by MOVEMENT with a value of: " + bestState.value);
+					bestState = temp;
 				}
-				setGrid(defGrid,grid);
+
+				setGrid(defGrid, grid);
 				pieces = getAIPieces();
-				phalanx=[];
+				phalanx = [];
 			}
 		}
 	}
 
 	setGrid(defGrid,grid);
 
-	//PHALANX CHECKING =====================
-	for(var i=0; i<4; i++){
-		var sameDir=[];
-		for (var p=0; p<6; p++){
-			if(pieces[p].rot==i){
+	// PHALANX CHECKING =====================
+	for(var i = 0; i < 4; i++){
+		var sameDir = [];
+		for (var p = 0; p < 6; p++){
+			if(pieces[p].rot == i){
 				sameDir.push(pieces[p]);
 			}
 		}
-		if(sameDir.length>1){
+		if(sameDir.length > 1){
 			var combinations = getCombinations(sameDir);
 			combinations.forEach(function(e){
 				phalanx = [];
 				phalanx = e.slice(0);
-				if(phalanx.length>1 && isPhalanx()){
-					for(var rot=0;rot<4;rot++){
-						if(rot!=i){
+				if(phalanx.length > 1 && isPhalanx()){
+					for(var rot = 0; rot < 4; rot++){
+						if(rot != i){
 							rotatePiece(phalanx[0].row, phalanx[0].col, rot);
 							pieces = getAIPieces();
 							var temp = copyState(defaultState);
-							getValue(temp,pieces);
-							if(temp.value>bestState.value){
-								bestState=temp;
-								// console.log("Found better state by ROTATING A PHALANX with a value of: " + bestState.value);
+							getValue(temp, pieces);
+							if(temp.value > bestState.value){
+								bestState = temp;
 							}
-							setGrid(defGrid,grid);
+							setGrid(defGrid, grid);
 							pieces = getAIPieces();
 						}
 					}
-					setGrid(defGrid,grid);
+					setGrid(defGrid, grid);
 					pieces = getAIPieces();
-					var rot = phalanx[0].rot;
 
-					var move = getMoveArguments(phalanx[0],rot);
-					movePiece(move.pRow,move.pCol,move.tRow,move.tCol,true);
+					var rot = phalanx[0].rot;
+					var move = getMoveArguments(phalanx[0], rot);
+					movePiece(move.pRow, move.pCol, move.tRow, move.tCol, true);
 
 					phalanx = e.slice(0);
 					pieces = getAIPieces();
 
 					var temp = copyState(defaultState);
-					getValue(temp,pieces);
-					if(temp.value>bestState.value){
-						bestState=temp;
-						// console.log("Found better state by MOVING A PHALANX with a value of: " + bestState.value);
+					getValue(temp, pieces);
+					if(temp.value > bestState.value){
+						bestState = temp;
 					}
-					setGrid(defGrid,grid);
+					setGrid(defGrid, grid);
 					pieces = getAIPieces();
 				}
 			});
 		}
 	}
 
-	//AFTER ALL CHECKING IS DONE ===========
-	setGrid(bestState.board,grid);
-	phalanx=[];
+	// AFTER ALL CHECKING IS DONE ===========
+	setGrid(bestState.board, grid);
+	phalanx = [];
 	useAction();
 	pushState();
 }
 
-function getValue(state,pieces){
-	for (var i=0; i<6; i++){
+function getValue(state, pieces){
+	var val;
+	for (var i = 0; i < pieces.length; i++) {
+		var piece = pieces[i];
+		val = 0;
 
-		var val = 0;
+		// Piece on Board
+		if(piece.kind != 3) {
+			val += aiP.values.ownPiecesOnBoard;
+		}
 
-		//Piece on Board
-		if(pieces[i].kind!=3){ val+=aiP.values.ownPiecesOnBoard; }
-
-		//Adjacent Check
+		// Adjacent Check
 		var adj = [];
-		if(pieces[i].row-1>-1){ adj[0] = grid[pieces[i].row-1][pieces[i].col]; }//N
-		if(pieces[i].col+1<grid[pieces[i].row].length){ adj[1] = grid[pieces[i].row][pieces[i].col+1]; } //E
-		if(pieces[i].row+1<grid.length){ adj[2] = grid[pieces[i].row+1][pieces[i].col]; } //S
-		if(pieces[i].col-1>0){ adj[3] = grid[pieces[i].row][pieces[i].col-1]; }//W
+		if(piece.row - 1 > -1)                     { adj[0] = grid[piece.row-1][piece.col]; } // Up
+		if(piece.col + 1 < grid[piece.row].length) { adj[1] = grid[piece.row][piece.col+1]; } // Right
+		if(piece.row + 1 < grid.length)            { adj[2] = grid[piece.row+1][piece.col]; } // Down
+		if(piece.col - 1 > 0)                      { adj[3] = grid[piece.row][piece.col-1]; } // Left
 
-		for(var j=0; j<4; j++){
-			if(adj[j]!=undefined && adj[j].kind!=-1 && adj[j].player>-1){
-				//Own
-				if(adj[j].player==pieces[i].player){
-					if(adj[j].rot==pieces[i].rot){ val+=aiP.values.ownAdjInPhalanx; }//In Phalanx
-					else{ val+=aiP.values.ownAdjNotInPhalanx; }//Not in Phalanx
+		for(var j = 0; j < 4; j++) {
+
+			if(adj[j] != undefined && adj[j].kind != -1 && adj[j].player >- 1) {
+				// Own
+				if(adj[j].player == piece.player){
+					if(adj[j].rot == piece.rot) {
+						// In Phalanx
+						val += aiP.values.ownAdjInPhalanx;
+					} else {
+						// Not in Phalanx
+						val += aiP.values.ownAdjNotInPhalanx;
+					}
 				}
 
-				//Ally
-				else if(Math.abs((adj[j].player-pieces[i].player)%2)==0){
-					if(adj[j].rot==pieces[i].rot){val+=aiP.values.allyAdjSameDir;} //Same Dir
-					else{val+=aiP.values.allyAdjDiffDir;} //Diff Dir
+				// Ally
+				else if(Math.abs( (adj[j].player - piece.player) % 2 ) == 0) {
+					if(adj[j].rot == piece.rot) { val += aiP.values.allyAdjSameDir; } // Same Dir
+					else{ val += aiP.values.allyAdjDiffDir; } // Diff Dir
 				}
 
-				//Enemy
-				else{
-					if(pieces[i].rot == j && Math.abs((adj[j].rot-j)%2)==0){
-						val+=aiP.values.enemyAdjBlocked; //Facing eachother.
+				// Enemy
+				else {
+					if(piece.rot == j && Math.abs((adj[j].rot-j) % 2) == 0) {
+						val += aiP.values.enemyAdjBlocked; // Facing eachother.
 					}
-					else if(pieces[i].rot==j){
-						//If piece is facing adj, but they aren't facing eachother, then adj is routable
-						if(gameMan.actions>1){ val+=aiP.values.enemyAdjRoutable[0]; }
-						else{ val+=aiP.values.enemyAdjRoutable[1]; }
+					else if(piece.rot == j) {
+						// If piece is facing adj, but they aren't facing eachother, then adj is routable
+						if(gameMan.actions > 1) {
+							val+= aiP.values.enemyAdjRoutable[0];
+						} else {
+							val+= aiP.values.enemyAdjRoutable[1];
+						}
 					}
-					else if(Math.abs((adj[j].rot-j)%2)==0){
-						//Else if we're not facing adjacent, check if he's facing us.
-						if(gameMan.actions>1){ val+=aiP.values.enemyAdjCanRoutMe[0]; }
-						else{ val+=aiP.values.enemyAdjCanRoutMe[1];}
+					else if(Math.abs( (adj[j].rot - j) % 2 ) == 0) {
+						// Else if we're not facing adjacent, check if he's facing us.
+						if(gameMan.actions > 1) {
+							val+=aiP.values.enemyAdjCanRoutMe[0];
+						} else {
+							val+=aiP.values.enemyAdjCanRoutMe[1];
+						}
 					}
-					else{
-						//Adjacent but not facing each other.
-						if(gameMan.actions>1){ val+=aiP.values.enemyAdjBothNotFacing[0]; }
-						else { val+=aiP.values.enemyAdjBothNotFacing[1]; }
+					else {
+						// Adjacent but not facing each other.
+						if(gameMan.actions > 1) {
+							val+=aiP.values.enemyAdjBothNotFacing[0];
+						} else {
+							val+=aiP.values.enemyAdjBothNotFacing[1];
+						}
 					}
 				}
 			}
 		}
-		val+=aiP.values.perSquareDistanceFromGoal*getDistanceFromGoal(pieces[i].row,pieces[i].col,gameMan.player);
-		state.value+=val; //Adds piece value to total value
+
+		val += aiP.values.perSquareDistanceFromGoal * getDistanceFromGoal(piece.row, piece.col, gameMan.player);
+		state.value += val; // Adds piece value to total value
 	}
-	//Add points for every routed enemy piece
+
+	// Add points for every routed enemy piece
 	for (var row = 0; row < 15; ++row) {
 		for (var col = 0; col < 21; ++col) {
-			if (grid[row][col].player>-1 && Math.abs(gameMan.player-grid[row][col].player)%2!=0 && grid[row][col].kind==3) {
-				state.value+=aiP.values.perRoutedEnemyPiece;
+			var cell = grid[row][col];
+			if (cell.player >- 1 && Math.abs(gameMan.player - cell.player) % 2 != 0 && cell.kind == 3) {
+				state.value += aiP.values.perRoutedEnemyPiece;
 			}
 		}
 	}
@@ -279,43 +299,44 @@ function copyState(original){
 	var o = original.board;
 	var s = newState();
 	var b = new Array(15);
+
 	for (var row = 0; row < 15; ++row){
-		b[row]=new Array(21);
+		b[row] = new Array(21);
 		for (var col = 0; col < 21; ++col){
 			var p = {}
-			p.row=row;
-			p.col=col;
-			p.checked=o[row][col].checked;
-			p.player=o[row][col].player;
-			p.kind=o[row][col].kind;
-			p.city=o[row][col].city;
-			p.rot=o[row][col].rot;
-			p.ring=o[row][col].ring;
-			p.prompt=o[row][col].prompt;
-			b[row][col]=p;
+			p.row       = row;
+			p.col       = col;
+			p.checked   = o[row][col].checked;
+			p.player    = o[row][col].player;
+			p.kind      = o[row][col].kind;
+			p.city      = o[row][col].city;
+			p.rot       = o[row][col].rot;
+			p.ring      = o[row][col].ring;
+			p.prompt    = o[row][col].prompt;
+			b[row][col] = p;
 		}
 	}
-	s.board=b;
+
+	s.board = b;
 	return s;
 }
 
 function setGrid(gFrom, gTo){
 	for (var row = 0; row < 15; ++row){
 		for (var col = 0; col < 21; ++col){
-			gTo[row][col].row=row;
-			gTo[row][col].col=col;
-			gTo[row][col].checked=gFrom[row][col].checked;
-			gTo[row][col].player=gFrom[row][col].player;
-			gTo[row][col].kind=gFrom[row][col].kind;
-			gTo[row][col].city=gFrom[row][col].city;
-			gTo[row][col].rot=gFrom[row][col].rot ;
-			gTo[row][col].ring=gFrom[row][col].ring;
-			gTo[row][col].prompt=gFrom[row][col].prompt;
+			gTo[row][col].row     = row;
+			gTo[row][col].col     = col;
+			gTo[row][col].checked = gFrom[row][col].checked;
+			gTo[row][col].player  = gFrom[row][col].player;
+			gTo[row][col].kind    = gFrom[row][col].kind;
+			gTo[row][col].city    = gFrom[row][col].city;
+			gTo[row][col].rot     = gFrom[row][col].rot ;
+			gTo[row][col].ring    = gFrom[row][col].ring;
+			gTo[row][col].prompt  = gFrom[row][col].prompt;
 		}
 	}
 }
 
-// get distance to goal - to be used in ai evaluation
 function getDistanceFromGoal(row, col, player) {
 
 	if (grid[row][col].kind == 3) {
@@ -415,20 +436,20 @@ function storeGrid(){
 		pGrid[row] = new Array(21);
 		for (var col = 0; col < 21; ++col) {
 			var cell = {
-				checked: grid[row][col].checked,
-				player : grid[row][col].player,
-				kind   : grid[row][col].kind,
-				city   : grid[row][col].city,
-				rot    : grid[row][col].rot,
-				ring   : grid[row][col].ring
+				checked : grid[row][col].checked,
+				player  : grid[row][col].player,
+				kind    : grid[row][col].kind,
+				city    : grid[row][col].city,
+				rot     : grid[row][col].rot,
+				ring    : grid[row][col].ring
 			}
 			pGrid[row][col] = cell;
 		}
 	}
-	defGrid=pGrid;
+	defGrid = pGrid;
 }
 
-function getAIPieces(){
+function getAIPieces() {
 	var p = [];
 	for (var row = 0; row < 15; ++row) {
 		for (var col = 0; col < 21; ++col) {
@@ -455,28 +476,26 @@ function getCombinations(pieces) {
 }
 
 function checkIfPiecesCanWin(pieces){
-	for (var i = 0; i<6; i++){
+	for (var i = 0; i < 6; i++) {
 		var d = getDistanceFromGoal(pieces[i].row,pieces[i].col,gameMan.player);
-		if(d<=2){
-			if(d==1){
-				//1 move
-				// console.log("1 space away.");
+		if(d <= 2){
+			if(d == 1){
+				// 1 move
 				return true;
 			}
-			else if(d==2){
-				//2 moves
-				// console.log("2 spaces away.");
+			else if(d == 2) {
+				// 2 moves
 				return true;
 			}
 		}
 	}
-
 	return false;
 }
 
-function getDirection(dir){
+function getDirection(dir) {
 	var incRow = 0;
 	var incCol = 0;
+
 	switch(dir){
 	case 0:
 		incRow = -1;
@@ -495,13 +514,13 @@ function getDirection(dir){
 		incCol = -1;
 		break;
 	}
-	return {row:incRow,col:incCol};
+	return { row: incRow, col: incCol};
 }
 
-function getMoveArguments(p,dir){
+function getMoveArguments(p, dir) {
 	var d = getDirection(dir);
-	var newRow = p.row+d.row;
-	var newCol = p.col+d.col;
+	var newRow = p.row + d.row;
+	var newCol = p.col + d.col;
 	return {
 		pRow : p.row,
 		pCol : p.col,
@@ -510,7 +529,7 @@ function getMoveArguments(p,dir){
 	}
 }
 
-function getRallyArguments(p,target){
+function getRallyArguments(p, target) {
 	return {
 		pRow : p.row,
 		pCol : p.col,
